d523c5fdfd6be716cd279ed839d44c5ff4ff3876
theci <p16331751@gmail.com>
2024-12-10 01:39:31 +0900

    2024-12-10

--- myproj/blog/api.py
+++ myproj/blog/api.py
@@ -1,68 +1,49 @@
+from core.mixins import (ActionBasedViewSetMixin, JSONResponseWrapperMixin,
+                         PermissionDebugMixin, TestFuncPermissionMixin)
+from core.pagination import make_pagination_class
+from core.permissions import (IsAuthorOrReadonly, PermitSafeMethods,
+                              make_drf_permission_class)
 from django.db.models import Model
 from django.shortcuts import get_object_or_404
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view
-from rest_framework.generics import (
-    ListAPIView,
-    RetrieveAPIView,
-    CreateAPIView,
-    UpdateAPIView,
-    DestroyAPIView,
-)
-from rest_framework.pagination import (
-    PageNumberPagination,
-    LimitOffsetPagination,
-    CursorPagination,
-)
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
+from rest_framework.pagination import (CursorPagination, LimitOffsetPagination,
+                                       PageNumberPagination)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import BrowsableAPIRenderer, JSONRenderer
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.utils.serializer_helpers import ReturnList, ReturnDict
+from rest_framework.utils.serializer_helpers import ReturnDict, ReturnList
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
-from core.mixins import (
-    JSONResponseWrapperMixin,
-    PermissionDebugMixin,
-    TestFuncPermissionMixin,
-    ActionBasedViewSetMixin,
-)
-from core.pagination import make_pagination_class
-from core.permissions import (
-    IsAuthorOrReadonly,
-    make_drf_permission_class,
-    PermitSafeMethods,
-)
 from .models import Post, Todo
-from .serializers import (
-    PostSerializer,
-    PostListSerializer,
-    PostDetailSerializer,
-    TodoSerializer,
-)
+from .serializers import (PostDetailSerializer, PostListSerializer,
+                          PostSerializer, TodoSerializer)
 
-
-# @api_view(["GET"])
+# @api_view(["GET"]) # 게시글 목록을 조회
 # def post_list(request: Request) -> Response:
-#     post_qs = Post.objects.all().defer("content").select_related("author")
+#     post_qs = Post.objects.all().defer("content").select_related("author") # content 필드를 쿼리에서 제외하고 author를 미리 조회하여 효율성을 높입니다.
 #
 #     serializer = PostListSerializer(instance=post_qs, many=True)
-#     list_data: ReturnList = serializer.data
+#     list_data: ReturnList = serializer.data # 직렬화하여 응답을 반환
 #
 #     return Response(list_data)
 
-
+# 게시글 목록 조회
 # class PostListAPIView(JSONResponseWrapperMixin, PermissionDebugMixin, ListAPIView):
-#     queryset = PostListSerializer.get_optimized_queryset()
+#     queryset = PostListSerializer.get_optimized_queryset() # 최적화된 쿼리셋을 설정
 #     serializer_class = PostListSerializer
 #
 #
 # post_list = PostListAPIView.as_view()
 #
-#
+# # 특정 게시글을 조회하는 API
 # # @api_view(["GET"])
-# # def post_detail(request: Request, pk: int) -> Response:
+# # def post_detail(request: Request, pk: int) -> Response: # pk로 전달된 게시글의 ID를 통해 게시글을 조회
 # #     post = get_object_or_404(Post, pk=pk)
 # #
 # #     serializer = PostDetailSerializer(instance=post)
@@ -70,22 +51,22 @@ from .serializers import (
 # #
 # #     return Response(detail_data)
 #
-#
+# 게시글 생성 API
 # class PostRetrieveAPIView(
 #     JSONResponseWrapperMixin, PermissionDebugMixin, RetrieveAPIView
 # ):
-#     queryset = PostDetailSerializer.get_optimized_queryset()
-#     serializer_class = PostDetailSerializer
+#     queryset = PostDetailSerializer.get_optimized_queryset() 
+#     serializer_class = PostDetailSerializer 
 #
 #
 # post_detail = PostRetrieveAPIView.as_view()
 #
 #
 # class PostCreateAPIView(PermissionDebugMixin, CreateAPIView):
-#     serializer_class = PostSerializer
-#     permission_classes = [IsAuthenticated]
+#     serializer_class = PostSerializer # 게시글 데이터를 직렬화
+#     permission_classes = [IsAuthenticated] # 인증된 사용자만 게시글을 생성
 #
-#     def perform_create(self, serializer):
+#     def perform_create(self, serializer): # 게시글이 저장될 때 author 필드를 요청한 사용자(request.user)로 설정
 #         serializer.save(author=self.request.user)
 #
 #
